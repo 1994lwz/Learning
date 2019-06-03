7c7063559724d557bdfc3857d9f4a4ec6d927ece
Wanzhuo Li <wal@synaptics.com>
2019-06-03 10:59:11 +0800

    modify test.py

--- Python/test.py
+++ Python/test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
 import math
 
 def quadratic(a, b, c):
@@ -19,9 +18,8 @@ elif quadratic(1, 3, -4) != (1.0, -4.0):
     print("测试失败")
 else:
     print("测试成功")
-"""
 
-def product(x, y = 0, *args):
+def product(x, y = 1, *args):
     m = x * y
     #print(args)
     for n in args:
@@ -34,7 +32,7 @@ print('product(5, 6) =', product(5, 6))
 print('product(5, 6, 7) =', product(5, 6, 7))
 print('product(5, 6, 7, 9) =', product(5, 6, 7, 9))
 if product(5) != 5:
-    print('测试成功!')
+    print('测试失败!')
 elif product(5, 6) != 30:
     print('测试失败!')
 elif product(5, 6, 7) != 210:
